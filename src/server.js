692214628f72e67ad0f893c6aea7ad3f113558a6
Erick Gledson <ericksilva@outlook.com>
2025-03-04 10:10:18 -0300

    add: server file

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const sequelize = require('./config/database');
+const redis = require('./config/redis');
+const app = require('./app');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const waitForRedis = () => {
+  return new Promise((resolve, reject) => {
+    if (redis.status === 'ready') {
+      return resolve();
+    }
+
+    redis.once('ready', resolve);
+    redis.once('error', reject);
+  });
+};
+
+Promise.all([sequelize.sync(), waitForRedis()])
+  .then(() => {
+    app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
+  })
+  .catch((err) => {
+    console.error('❌ Ocorreu um erro ao subir a aplicação:', err);
+    process.exit(1);
+  });
